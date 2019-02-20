12f8b79bea4350c642795717643aa42b744d097d
shubhampatel <shubham.patel732@gmail.com>
2019-02-20 22:46:39 +0530

    My project setup completed successfully.

---
+++ rrbackend/src/main/java/com/spr/rrbackend/App.java
@@ -0,0 +1,13 @@
+package com.spr.rrbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
